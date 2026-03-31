9932aa1f0dad6447cb1ef82d2165e58d59a2630b
AlekseyKurnakov <kurnakov.a.v@mail.ru>
2026-03-31 07:24:18 +0300

    Character

--- src/main/java/org/example/Reversed.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Reversed {
-
-    public static String reverse(String input) {
-        var reversed = new StringBuilder(input);
-        return reversed.reverse().toString();
-    }
-}
