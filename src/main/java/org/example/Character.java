140d9b53040308e17dd2207d92f84cb11e8171bf
AlekseyKurnakov <kurnakov.a.v@mail.ru>
2026-04-02 08:02:12 +0300

    Caracter s3

--- src/main/java/org/example/Character.java
+++ src/main/java/org/example/Character.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.states.NormalState;
+import org.example.states.PoisonedState;
 import org.example.states.State;
 
 public class Character{
@@ -9,6 +11,7 @@ public class Character{
     private int currentHealth;
     private final int baseAttackPower;
     private int currentAttackPower;
+    private State state;
 
     public Character(String name, int health, int attack) {
         this.name = name;
@@ -16,5 +19,46 @@ public class Character{
         currentHealth = health;
         baseAttackPower = attack;
         currentAttackPower = attack;
+        state = new NormalState(this);
+        state.enter();
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+    public int getBaseAttackPower() {
+        return baseAttackPower;
+    }
+    public int getCurrentAttackPower() {
+        return currentAttackPower;
+    }
+    public State getState() {
+        return state;
+    }
+    public void setCurrentAttackPower(int attack) {
+        currentAttackPower = attack;
+    }
+    public void takeDamage(int damage){
+        if (currentHealth - damage <= 0) {
+            throw new IllegalStateException();
+        }
+        currentHealth = currentHealth - damage;
+    }
+
+    public void poison() {
+        state.exit();
+        state = new PoisonedState(this);
+        state.enter();
+    }
+    public void undoSpells() {
+        state.exit();
+        state = new NormalState(this);
+        state.enter();
     }
 }
--- src/main/java/org/example/states/PoisonedState.java
+++ src/main/java/org/example/states/PoisonedState.java
@@ -1,14 +1,36 @@
 package org.example.states;
+import org.example.Character;
 
 public class PoisonedState implements State {
-    private Character character;
+    private final Character character;
+
+    public PoisonedState(Character character) {
+        this.character = character;
+    }
 
     @Override
     public String getTitle() {
-        System.out.println(" is poisoned");
+        return "Poisoned";
     }
-    void enter();
-    void exit();
-    void update();
-    int handleAttack();
+
+    @Override
+    public void enter() {
+        System.out.println(character.getName() + " is poisoned");
+        character.setCurrentAttackPower(character.getBaseAttackPower() / 2);
+    }
+
+    @Override
+    public void exit() {
+        System.out.println(character.getName() + " is no longer poisoned");
+    }
+
+    @Override
+    public void update(){
+        character.takeDamage(5);
+    }
+
+    @Override
+    public int handleAttack() {
+        return character.getCurrentAttackPower();
+    }
 }
